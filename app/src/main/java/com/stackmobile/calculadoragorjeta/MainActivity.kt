31a073bd79deabd0946383c93f42a2d34f8e0108
Pablo Vinícius Sousa Silva <48532322+PabloViniciusSS@users.noreply.github.com>
2023-02-28 18:10:58 -0300

    feito

--- app/src/main/java/com/stackmobile/calculadoragorjeta/MainActivity.kt
+++ app/src/main/java/com/stackmobile/calculadoragorjeta/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.stackmobile.calculadoragorjeta
 
+import android.content.Context
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.google.android.material.snackbar.Snackbar
 import com.stackmobile.calculadoragorjeta.databinding.ActivityMainBinding
 import java.text.NumberFormat
@@ -19,7 +23,24 @@ class MainActivity : AppCompatActivity() {
 
         binding.btCalcular.setOnClickListener { calculateTip() }
 
+
+        binding.costOfServiceEditText.setOnKeyListener { view, keyCode, _ -> handleKeyEvent(view, keyCode)
         }
+
+        }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
+    }
+
+
 
         fun calculateTip() {
 
@@ -27,7 +48,7 @@ class MainActivity : AppCompatActivity() {
             val cost = stringInTextField.toDoubleOrNull()
 
             if (cost == null) {
-                binding.tipResult.text = ""
+                println("")
                 return
             }
 
